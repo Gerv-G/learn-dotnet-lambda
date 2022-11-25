35211e6fc0bb7965d791bcf6b11694e7fef773ee
Gerv-G <25771885+Gerv-G@users.noreply.github.com>
2022-11-25 17:11:21 +0800

    chore: Refactor application configuration and DI

---
+++ GreetingApi/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace GreetingApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration{ get; }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment webHostEnvironment)
+    {
+        if (webHostEnvironment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
